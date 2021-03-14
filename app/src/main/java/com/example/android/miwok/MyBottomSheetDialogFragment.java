497bf2c3cb90b80b0b51e3234669d97b928c3f01
artem <ar4er777@gmail.com>
2021-03-14 04:33:09 +0200

    bottom_sheet_modal.xml created with drawables and text. Starting to implement BottomSheetDialogFragment

---
+++ app/src/main/java/com/example/android/miwok/MyBottomSheetDialogFragment.java
@@ -0,0 +1,6 @@
+package com.example.android.miwok;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class MyBottomSheetDialogFragment extends BottomSheetDialogFragment {
+}
