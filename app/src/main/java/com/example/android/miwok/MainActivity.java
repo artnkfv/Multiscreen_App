58a20e73cb48c7e1eecefa27bdbd7a1e5be65941
artem <ar4er777@gmail.com>
2021-03-15 02:24:28 +0200

    MyBottomSheetDialogFragment.java added, in MainActivity.java NavigationOnClickListener implemented(now on click of menu icon in bottomAppBar ,modal bottom sheet is appear)

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.miwok;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -25,10 +26,12 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.widget.NestedScrollView;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
@@ -138,6 +141,8 @@ public class MainActivity extends AppCompatActivity {
         bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                BottomSheetDialogFragment bottomSheetDialogFragment = new MyBottomSheetDialogFragment();
+                bottomSheetDialogFragment.show(getSupportFragmentManager(),"Bottom Sheet");
 
 
             }
--- app/src/main/java/com/example/android/miwok/MyBottomSheetDialogFragment.java
+++ app/src/main/java/com/example/android/miwok/MyBottomSheetDialogFragment.java
@@ -1,6 +1,19 @@
 package com.example.android.miwok;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class MyBottomSheetDialogFragment extends BottomSheetDialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.bottom_sheet_modal, container, false);
+
+        return rootView;
+    }
 }
